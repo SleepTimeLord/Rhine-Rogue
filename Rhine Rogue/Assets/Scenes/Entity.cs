cafb666adb41977dff44482a3596426ea164fe8c
Anonymoose-11 <140772542+Anonymoose-11@users.noreply.github.com>
2024-05-03 20:14:43 -0700

    Added PlayerEntity
    Added the PlayerEntity class.

--- Rhine Rogue/Assets/Scenes/Entity.cs
+++ Rhine Rogue/Assets/Scenes/Entity.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Entity : MonoBehaviour
 {
     public float health;
+    public Vector3Int position;
     public RaceType raceType;
     public int speed;
     public int initiative;
+    public int baseInitiative;
     public float attack;
+    public Tilemap tilemap;
     //public List<*something*> skills;
 
 }
