2ff4f4316586de8a3b733a566342792a8761838f
DyingRavioli <lijms181@gmail.com>
2024-09-02 22:16:12 -0400

    replaced grassblocks

--- Rhine Rogue/Assets/Scripts/MonsterDatabase.cs
+++ Rhine Rogue/Assets/Scripts/MonsterDatabase.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class MonsterDatabase : MonoBehaviour
 {
 
+    public List<Monster> monsterStorage = new List<Monster>();
+    public void SaveDatabase()
+    {
+        string monsterdata = JsonUtility.ToJson(monsterStorage);
+        string filePath = Application.persistentDataPath + "/MonsterDatabase.json";
+        System.IO.File.WriteAllText(filePath, monsterdata);
+        Debug.Log("Saved to" + Application.persistentDataPath);
+    }
+
 }
 
 [System.Serializable]
@@ -12,7 +21,9 @@ public class Monster{
     public RaceType type;
     public int health;
     public int mana;
-        
+    public int defense;
+    public int attack;
+    public List<StatusEffect> statuses = new List<StatusEffect>();
     public List<Ability> abilities = new List<Ability>();
 }
 
