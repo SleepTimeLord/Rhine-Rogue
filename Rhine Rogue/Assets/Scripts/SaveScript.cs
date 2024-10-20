f4f0cd0aabae4747e52832ba28da467874a32163
DyingRavioli <lijms181@gmail.com>
2024-10-20 12:30:04 -0400

    Monster Database & Dithering Array
    - added monster database
       -monsters folder
       -has scriptableobject called database that has functions to add and retrieve monsters
       -has array of all monsters
       -just put all monster assets into the monster folder
    -added dithering array
       -10 dither variations for sides
       -corner dithering coming later
    -added new test scenes to test dithering and lighting

--- Rhine Rogue/Assets/Scripts/SaveScript.cs
+++ Rhine Rogue/Assets/Scripts/SaveScript.cs
@@ -5,36 +5,62 @@ using UnityEngine;
 public class SaveScript : MonoBehaviour
 {
 
-    void Start(){
-        LoadJson();
-    }
-
     public PlayerData playerData = new PlayerData();
+    public PlayerSettings playerSettings = new PlayerSettings();
+
+    //JSON PATHS
+    string dataFilePath;
+    string settingsFilePath;
+
+    void Start()
+    {
+        //LoadJson();
+
+        dataFilePath = Application.persistentDataPath + "/PlayerData.json";
+        settingsFilePath = Application.persistentDataPath + "/PlayerSettings.json";
+    }
 
     public void SaveJson()
     {
         string plrData = JsonUtility.ToJson(playerData);
-        string filePath = Application.persistentDataPath + "/PlayerData.json";
-        System.IO.File.WriteAllText(filePath,plrData);
+        string settingsData = JsonUtility.ToJson(playerSettings);
+
+        System.IO.File.WriteAllText(dataFilePath,plrData);
+        System.IO.File.WriteAllText(settingsFilePath,settingsData);
+
         Debug.Log("Saved to" + Application.persistentDataPath);
     }
 
     public void LoadJson()
     {
-        string filePath = Application.persistentDataPath + "/PlayerData.json";
-        string plrData = System.IO.File.ReadAllText(filePath);
+        string plrData = System.IO.File.ReadAllText(dataFilePath);
+        string settingsData = System.IO.File.ReadAllText(settingsFilePath);
 
         Debug.Log(plrData);
+        Debug.Log(settingsData);
     }
 
 }
 
 [System.Serializable]
-public class PlayerData{
+public class PlayerData
+{
     public int coins;
     public int health;
-    public List<Monster> party = new List<Monster>();
+    public List<Entity> party = new List<Entity>();
 }
 
+public class PlayerSettings
+{
+    public int masterVolume;
+    public int musicVolume;
+    public int sfxVolume;
+    public int graphicsIndex;
+    public int antialiasingIndex;
+    public float renderScale;
+    public int resolutionIndex;
+    public bool vSync;
+    public bool fullScreen;
+}
 
 
